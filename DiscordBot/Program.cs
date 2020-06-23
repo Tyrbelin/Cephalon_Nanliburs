9a4139e63d61a6f2424ad4c855dfd276429cad0a
Minseung,Son <alstmd89@fakeeyes.co.kr>
2020-06-23 16:31:34 +0900

    Revert "Service 추가"
    This reverts commit 7234488cc7ce23a25a296c2978bb3b9304130b8e.

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -3,10 +3,6 @@ using System.Net;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
-using Discord.Commands;
-using Microsoft.Extensions.DependencyInjection;
-using System.Net.Http;
-using System.Threading;
 
 namespace DiscordBot
 {
@@ -23,43 +19,32 @@ namespace DiscordBot
         public async Task MainAsync()
         {
             Localize.Init();
-            using (var services = ConfigureServices())
-            {
-                _client = services.GetRequiredService<DiscordSocketClient>();
-                _client.Log += Log;
-                services.GetRequiredService<CommandService>().Log += Log;
-                //  You can assign your bot token to a string, and pass that in to connect.
-                //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-                //var token = Environment.GetEnvironmentVariable("BOT_TOKEN",EnvironmentVariableTarget.Machine);
-                var token = Environment.GetEnvironmentVariable("BOT_TOKEN");
-                // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-                // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-                // var token = File.ReadAllText("token.txt");
-                // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            _client = new DiscordSocketClient();
 
-                await _client.LoginAsync(TokenType.Bot, token);
-                await _client.StartAsync();
-                await services.GetRequiredService<CommandHandlingService>().InitializeAsync();
+            _client.Log += Log;
 
-                // Block this task until the program is closed.
-                await Task.Delay(Timeout.Infinite);
-            }
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            //var token = Environment.GetEnvironmentVariable("BOT_TOKEN",EnvironmentVariableTarget.Machine);
+            var token = Environment.GetEnvironmentVariable("BOT_TOKEN");
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client,new Discord.Commands.CommandService());
+            await _handler.InstallCommandsAsync();
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
         }
 
         private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
-        }
-
-        private ServiceProvider ConfigureServices()
-        {
-            return new ServiceCollection()
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<CommandService>()
-                .AddSingleton<CommandHandlingService>()
-                .AddSingleton<HttpClient>()
-                .BuildServiceProvider();
         }
     }
 }
